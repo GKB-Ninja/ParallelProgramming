87296d017d7d53fdb74f36bdac98697fdba1aa7a
Ahmet alp Özdemir <109877223+akoalp@users.noreply.github.com>
2026-03-29 16:38:02 +0300

    Update sequences_alp_ozdemir.py

--- Week03/sequences_alp_ozdemir.py
+++ Week03/sequences_alp_ozdemir.py
@@ -1,14 +1,13 @@
-def remove_duplicates(seq):
+def remove_duplicates(seq: list) -> list:
     res = set(seq)
     return res
 
-
-def list_counts(seq):
+def list_counts(seq : list) -> dict:
     counts = {}
     for i in seq:
         counts[i] = counts.get(i,0) + 1
     return counts
 
-
-def reverse_dict(d):    
-    return reversed(d)
+def reverse_dict(d: dict) -> dict: 
+    res = dict(reversed(list(d)))   
+    return res
